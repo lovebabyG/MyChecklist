9ef3b3918927390ae5b4d47676c9d87138a88877
Zhaofei Yin <zhaofeiyin@Zhaofeis-iMac.local>
2017-11-06 06:42:37 -0800

    second commit

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -19,6 +19,14 @@ class CheckListViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cell;
+    }
 
 
 }
