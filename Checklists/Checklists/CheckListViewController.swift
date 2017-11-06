0683554a6b1ce956e51b1b5f957221117c0c93a7
Zhaofei Yin <zhaofeiyin@Zhaofeis-iMac.local>
2017-11-06 06:23:09 -0800

    my first commit

---
+++ Checklists/Checklists/CheckListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Zhaofei Yin on 06.11.17.
+//  Copyright © 2017 Zhaofei Yin. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
