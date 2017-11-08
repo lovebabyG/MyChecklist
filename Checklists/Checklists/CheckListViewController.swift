edeb687bf2cb07092908b3b52acd9c0d15ea8bb6
Zhaofei Yin <zhaofei.yin@gmail.com>
2017-11-08 21:44:38 +0100

    commit something

--- Checklists/Checklists/CheckListViewController.swift
+++ Checklists/Checklists/CheckListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CheckListViewController : UITableViewController,
-                                AddItemViewControllerDelegate
+                                ItemDetailViewControllerDelegate
 {
    
     required init?(coder aDecoder: NSCoder) {
@@ -42,6 +42,8 @@ class CheckListViewController : UITableViewController,
         
         super.init(coder: aDecoder)
         
+        print("Documents folder is \(documentsDirectory())")
+        print("Data file path is \(dataFilePath())")
     }
     
     
@@ -92,11 +94,11 @@ class CheckListViewController : UITableViewController,
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddItem" {
             let navigationController = segue.destination as! UINavigationController
-            let controller = navigationController.topViewController as! AddItemViewController
+            let controller = navigationController.topViewController as! ItemDetailViewController
             controller.delegate = self
         } else if segue.identifier == "EditItem" {
             let navigationController = segue.destination as! UINavigationController
-            let controller = navigationController.topViewController as! AddItemViewController
+            let controller = navigationController.topViewController as! ItemDetailViewController
             controller.delegate = self
             if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
                 controller.itemToEdit = items[indexPath.row]
@@ -104,12 +106,14 @@ class CheckListViewController : UITableViewController,
         }
     }
     
-    func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+    func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController) {
         dismiss(animated: true, completion: nil)
     }
     
-    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
+    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem) {
         let newRowIndex = items.count
+        items.append(item)
+        
         let indexPath = IndexPath(row: newRowIndex, section: 0)
         let indexPaths = [indexPath]
         tableView.insertRows(at: indexPaths, with: .automatic)
@@ -117,7 +121,7 @@ class CheckListViewController : UITableViewController,
         dismiss(animated: true, completion: nil)
     }
     
-    func additemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem) {
+    func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem) {
         if let index = items.index(of: item) {
             let indexPath = IndexPath(row: index, section: 0)
             if let cell = tableView.cellForRow(at: indexPath){
@@ -126,6 +130,16 @@ class CheckListViewController : UITableViewController,
         }
         
         dismiss(animated: true, completion: nil)
+    }
+    
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory,
+                                             in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
     }
     
     func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
